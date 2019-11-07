d9168c2af26ac78fc434d919d86844a6a6088700
Moonslate <42197658+Moonslate@users.noreply.github.com>
2019-11-07 02:23:05 -0200

    new functions toBbitConverter

--- HM Studio/src/class_bit_converter.cpp
+++ HM Studio/src/class_bit_converter.cpp
@@ -1,6 +1,1 @@
 #include "class_bit_converter.hpp"
-
-//inline unsigned int BitConverter::ToInt32(const void* data, unsigned int index)
-//{
-//	
-//}
--- HM Studio/src/class_bit_converter.hpp
+++ HM Studio/src/class_bit_converter.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdint.h>
+#include <vector>
 
 class BitConverter
 {
@@ -10,4 +11,46 @@ private:
 
 public:
 	static inline uint32_t ToInt32(const void* data, unsigned int index) { return *(int*)(((char*)data) + index); };
+
+	static inline void Get2Bits(std::vector<uint8_t>& bits, const uint8_t byte)
+	{
+		bits.reserve(4);
+		uint8_t b = byte;
+
+		for (int i = 0; i < 4; ++i)
+		{
+			bits.push_back(b & 0b00000011);
+			b = b >> 2;
+		}
+	}
+	
+	static inline void Get4Bits(uint8_t& left, uint8_t& right, const uint8_t byte)
+	{
+		left = byte >> 4;
+		right = byte & 0x0f;
+	}
+
+	static inline void Get8Bits(std::vector<uint8_t>& bits, const uint8_t byte)
+	{
+		bits.resize(8);
+
+		uint8_t b = byte;
+
+		for (int i = 0; i < 8; ++i)
+		{
+			bits[i] = b & 0b00000001;
+			b = b >> 1;
+		}		
+	}
+
+	static inline void ReverseNibble(uint8_t& byte)
+	{
+		uint8_t left = 0;
+		uint8_t right = 0;
+
+		Get4Bits(left, right, byte);
+
+		byte = 0x0f & left;
+		byte = 0xf0 & right;
+	}
 };
