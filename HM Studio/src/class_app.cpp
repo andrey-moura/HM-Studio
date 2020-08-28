c15bf98917fdbf879666005ed52b3b9534c96bf9
Moonslate <42197658+Moonslate@users.noreply.github.com>
2020-08-28 13:18:53 -0300

    copying the release ini was not a good idea

--- HM Studio/src/class_app.cpp
+++ HM Studio/src/class_app.cpp
@@ -20,15 +20,6 @@ bool App::OnInit()
 	wxFileName fn(wxStandardPaths::Get().GetExecutablePath());
 	fn.SetExt(L"ini");
 
-#ifdef _DEBUG
-	//This was very annoying, the debug ini file always overrides the release one.
-	fn.SetName(L"HM Studio");
-	wxString release_file = fn.GetFullPath();
-	fn.SetName(L"HM Studio(d)");
-	wxString file = fn.GetFullPath();
-	wxCopyFile(release_file, file);
-#endif // _DEBUG
-
 	wxFileConfig* configs = new wxFileConfig(wxEmptyString, wxEmptyString, fn.GetFullPath());
 	configs->SetRecordDefaults();
 	configs->EnableAutoSave();	
